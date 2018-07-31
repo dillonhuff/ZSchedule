a4873e0bfa14fb90a5c4129dd6481ae75166dc38
Dillon Huff <dillonhuff@gmail.com>
2018-07-31 00:10:05 -0700

    Can actually get an optimization to print

--- src/schedule.cpp
+++ src/schedule.cpp
@@ -1,8 +1,31 @@
 #include "schedule.h"
 
+#include <iostream>
+
 #include "z3++.h"
 
+using namespace std;
 using namespace z3;
 
-void asapSchedule() {
+namespace ZSchedule {
+
+  void asapSchedule() {
+    context c;
+    optimize opt(c);
+    params p(c);
+    opt.set(p);
+    expr x = c.int_const("x");
+    expr y = c.int_const("y");
+    opt.add(10 >= x && x >= 0);
+    opt.add(10 >= y && y >= 0);
+    opt.add(x + y <= 11);
+    optimize::handle h1 = opt.maximize(x);
+
+    opt.check();
+
+    cout << "x = " << opt.lower(h1) << endl;
+
+    cout << "Got schedule" << endl;
+  }
+
 }
--- test/schedule_tests.cpp
+++ test/schedule_tests.cpp
@@ -1,9 +1,11 @@
 #include "catch.hpp"
 
+#include "schedule.h"
+
 namespace ZSchedule {
 
   TEST_CASE("Pure DFG schedule") {
-    
+    asapSchedule();
   }
 
 }
